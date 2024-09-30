68cc8745527ad90f004655d3dd650d73aed66816
Yugen <tronnyg@gmail.com>
2024-09-30 17:33:32 +0200

    Added SSL to server.js & Refactor .gitignore to exclude SSL certificate files

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-// Initialize Express app
-const server = express();
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 // Test route (for verifying server is up and running)
-server.get('/', (req, res) => {
-    res.status(200).json({ message: 'Customer International Payments Portal API is running' });
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Customer International Payments Portal API is running" });
 });
 
 //Only runs if the module is run directly (not in tests)
@@ -16,12 +18,26 @@ if (require.main === module) {
   dotenv.config();
   // Connect to MongoDB
   connectDB();
+
+  // Create an HTTPS server
+  const server = https.createServer(
+    {
+      key: fs.readFileSync("./keys/privatekey.pem"),
+      cert: fs.readFileSync("./keys/certificate.pem"),
+    },
+    app
+  );
+
   // Start the server
   const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
 
-// Export the server for testing
-module.exports = server;
+  // Export the server for production
+  module.exports = server;
+} else {
+  // Export the server for testing
+  const server = app;
+  module.exports = server;
+}
