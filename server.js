1420b48d794ec64992e98d99991faea2ef8b6982
Yugen <tronnyg@gmail.com>
2024-09-30 15:25:31 +0200

    Fixed Error Connecting to DB When Running Tests

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
-// Load environment variables from the .env file
-dotenv.config();
-
-console.log(process.env.MONGO_URI);
-
-// Connect to MongoDB
-connectDB();
-
 // Initialize Express app
 const server = express();
 
@@ -18,13 +10,17 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'Customer International Payments Portal API is running' });
 });
 
-// Start the server
+//Only runs if the module is run directly (not in tests)
 if (require.main === module) {
-    // Start the server only if this file is run directly (not in tests)
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+  // Load environment variables from the .env file
+  dotenv.config();
+  // Connect to MongoDB
+  connectDB();
+  // Start the server
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 
 // Export the server for testing
